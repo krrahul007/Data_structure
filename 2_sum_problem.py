41e1d0ddf5d2770fa8f769512d10b77516e2ec4d
Rahul kumar <93810628+krrahul007@users.noreply.github.com>
2021-12-08 12:24:35 +0530

    Add files via upload

---
+++ 2_sum_problem.py
@@ -0,0 +1,28 @@
+
+# coding: utf-8
+PROBLEM TARGET
+# In[9]:
+
+
+max= 10000000000
+def SumDigit(arr,n,target):
+    p1,p2=0, 0 
+    low, high, diff=0, n-1, max
+    
+    while high > low:
+        if abs(arr[low] + arr[high] - target) < diff:
+            p1 = low
+            p2 = high
+            diff = abs(arr[low] + arr[high] - target)
+        
+        if (arr[low] + arr[high]) > target:
+            high = high - 1
+        else:
+            low = low + 1
+    print('the pair is {} and {}'.format(arr[low],arr[high]))
+
+arr = [10,22,28,29,30,40]
+target = 54
+n = len(arr)
+SumDigit(arr,n,target)
+
